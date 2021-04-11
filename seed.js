6469996efaef14da3541e8af1e71ac9a8ceb2165
Aniruddha <aniruddha@cyberautic.com>
2021-04-11 17:45:15 +0530

    seed updated

--- seed.js
+++ seed.js
@@ -3,3 +3,9 @@ module.exports = {
 	"dev": "localhost:27017/users_api_demo_development",
 	"prod": "localhost:27017/users_api_demo_development"
 }
+
+
+User.create(first_name: 'marie', last_name: 'curie', email: 'mc@radiation.com')
+User.create(first_name: 'issac', last_name: 'newton', email: 'in@gravity.com')
+User.create(first_name: 'galileo', last_name: 'galilei', email: 'gg@astronomy.com')
+User.create(first_name: 'Aniruddha', last_name: 'Mirajkar', email: 'anim@gmail.com')
